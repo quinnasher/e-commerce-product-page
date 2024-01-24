7fc3cc41fe35db30ee231ba84435ab19d4206644
quinnasher <pauldivineagionoja@yahoo.com>
2024-01-24 23:10:01 +0100

    added hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,24 +2,36 @@ import cartIcon from "./images/icon-cart.svg";
 import menuIcon from "./images/icon-menu.svg";
 import imageAvatar from "./images/image-avatar.png";
 import logo from "./images/logo.svg";
+import closeIcon from "./images/icon-close.svg";
+import { useState } from "react";
 function Header() {
+  const [open, setOpen] = useState("hidden");
+  function openNav() {
+    setOpen("");
+  }
+
+  function closeNav() {
+    setOpen("hidden");
+  }
+
   return (
     <div
       className={
-        "bg-white font-700 w-375px md:w-1000px md:px-0 px-4 py-4 mx-auto "
+        "bg-white   font-700 w-375px md:w-1000px md:px-0 px-4 py-4 mx-auto "
       }
     >
       <header className={"flex justify-between items-center "}>
         <div className="flex items-centr gap-4">
-          <button className={"md:hidden"}>
+          <button onClick={openNav} className={"md:hidden"}>
             <img className={" w-6"} src={menuIcon} alt="menu Icon" />
+          </button>
+          <button onClick={closeNav} className={`z-30 w-20 absolute ${open}`}>
+            <img className={"md:hidden object-cover"} src={closeIcon} alt="" />
           </button>
           <img className={"w-full"} src={logo} alt="logo" />
         </div>
         <nav
-          className={
-            "bg-white h-screen  w-72 absolute bottom-0 top-0 left-0 p-4 md:p-0 md:h-full md:w-full  md:relative  md:ml-16"
-          }
+          className={`bg-white  transition ${open}  duration-500 h-screen  w-3/5 absolute bottom-0 top-0 left-0 p-4 md:block md:p-0 md:h-full md:w-full  md:relative  md:ml-16`}
         >
           <ul
             className={
